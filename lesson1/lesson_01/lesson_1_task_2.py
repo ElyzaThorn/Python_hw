08d9c245428105159bcc4ee0d2bf94155d854848
ElyzaThorn <mayakovskaya.2020@bk.ru>
2025-05-12 13:07:52 +0300

    new

---
+++ lesson1/lesson_01/lesson_1_task_2.py
@@ -0,0 +1,3 @@
+my_age = 27
+my_age = my_age + 3
+print("Мой возраст через 3 года будет:", my_age)
---
+++ lesson1/lesson_01/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = input("Введите ваше имя: Elizaveta")
+last_name = input("Введите вашу фамилию: Khachatryan")
+print("Вас зовут: {last_name} {first_name}")
